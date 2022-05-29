8336e6a8c4c2aab61dbd6a4447f60925b9fe1134
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2022-05-29 11:32:03 +0200

    some images and hero area

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,23 +41,8 @@ export const Header = () => {
       <div className="navbar navbar-dark bg-dark shadow-sm">
         <div className="container">
           <a href="#" className="navbar-brand d-flex align-items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              aria-hidden="true"
-              className="me-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-              <circle cx="12" cy="13" r="4" />
-            </svg>
-            <strong>Album</strong>
+            <img src="/src/img/Logo.png" />
+            <strong className="ms-2">EN for IT</strong>
           </a>
           <button
             className="navbar-toggler"
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -8,18 +8,16 @@ export const MainPage = () => {
       <section className="py-5 text-center container">
         <div className="row py-lg-5">
           <div className="col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light">Album example</h1>
+            <h1 className="fw-light">
+              Yuliya Burlai English for IT online lessons
+            </h1>
             <p className="lead text-muted">
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don’t simply skip over it entirely.
+              I’m offering a friendly online english lessons for all levels :)
+              First lesson is always free - check me out!
             </p>
             <p>
               <a href="#" className="btn btn-primary my-2">
-                Main call to action
-              </a>
-              <a href="#" className="btn btn-secondary my-2">
-                Secondary action
+                Grab your free first lesson
               </a>
             </p>
           </div>
@@ -31,6 +29,12 @@ export const MainPage = () => {
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             <div className="col">
               <div className="card shadow-sm">
+                <img
+                  width="100%"
+                  height="100%"
+                  className="image-responsive"
+                  src="src/img/class.png"
+                />
                 <div className="card-body">
                   <p className="card-text">
                     This is a wider card with supporting text below as a natural
@@ -59,6 +63,12 @@ export const MainPage = () => {
             </div>
             <div className="col">
               <div className="card shadow-sm">
+                <img
+                  width="100%"
+                  height="100%"
+                  className="image-responsive"
+                  src="src/img/work.png"
+                />
                 <div className="card-body">
                   <p className="card-text">
                     This is a wider card with supporting text below as a natural
@@ -87,6 +97,12 @@ export const MainPage = () => {
             </div>
             <div className="col">
               <div className="card shadow-sm">
+                <img
+                  width="100%"
+                  height="100%"
+                  className="image-responsive"
+                  src="src/img/vault.png"
+                />
                 <div className="card-body">
                   <p className="card-text">
                     This is a wider card with supporting text below as a natural
@@ -113,7 +129,6 @@ export const MainPage = () => {
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
