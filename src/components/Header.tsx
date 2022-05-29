c40e261752c9a9694d90737ebcc9231dfd22fdd6
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2022-05-29 19:48:01 +0200

    add fonts + Hero area

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,61 +2,14 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <header>
-      <div className="collapse bg-dark" id="navbarHeader">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-8 col-md-7 py-4">
-              <h4 className="text-white">About</h4>
-              <p className="text-muted">
-                Add some information about the album below, the author, or any
-                other background context. Make it a few sentences long so folks
-                can pick up some informative tidbits. Then, link them off to
-                some social networking sites or contact information.
-              </p>
-            </div>
-            <div className="col-sm-4 offset-md-1 py-4">
-              <h4 className="text-white">Contact</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#" className="text-white">
-                    Follow on Twitter
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-white">
-                    Like on Facebook
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-white">
-                    Email me
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="navbar navbar-dark bg-dark shadow-sm">
-        <div className="container">
-          <a href="#" className="navbar-brand d-flex align-items-center">
-            <img src="/src/img/Logo.png" />
-            <strong className="ms-2">EN for IT</strong>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarHeader"
-            aria-controls="navbarHeader"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </div>
+<header className="d-flex justify-content-center py-3 header">
+      <ul className="nav nav-pills">
+        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+      </ul>
     </header>
   );
 };
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,26 +1,32 @@
 export const MainPage = () => {
   return (
     <div>
-      <section className="py-5 text-center container">
-        <div className="row py-lg-5">
-          <div className="col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light">
-              Yuliya Burlai English for IT online lessons
-            </h1>
-            <p className="lead text-muted">
-              I’m offering a friendly online english lessons for all levels :)
-              First lesson is always free - check me out!
-            </p>
-            <p>
-              <a href="#" className="btn btn-primary my-2">
-                Grab your free first lesson
-              </a>
-            </p>
+      <section className="bg-light">
+        <div className="text-center container">
+          <div className="row py-lg-3">
+            <div className="col-md-6 mx-auto d-flex align-items-center">
+              <div className="">
+                <h1 className="fw-light">
+                  Yuliya Burlai English for IT online lessons
+                </h1>
+                <p className="lead text-muted">
+                  I’m offering friendly online English lessons for all levels :)
+                </p>
+                <p>
+                  <a href="#" className="btn my-2 cta-button">
+                    Contact me for details
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <img className="hero-image" src="/src/img/portrait.png" />
+            </div>
           </div>
         </div>
       </section>
 
-      <div className="album py-5 bg-light">
+      <div className="album py-5">
         <div className="container">
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             <div className="col">
@@ -34,7 +40,12 @@ export const MainPage = () => {
                 <div className="card-body">
                   <h3>Education</h3>
                   <p className="card-text">
-                    While I have the classic combo of MA in English, a non-degree year of studying in the USA plus CELTA, I consider my experience with other languages just as important. At some point in my life I learned & dropped German & Chinese but continued learning & using French, Spanish & Polish.
+                    While I have the classic combo of MA in English, a
+                    non-degree year of studying in the USA plus CELTA, I
+                    consider my experience with other languages just as
+                    important. At some point in my life I learned & dropped
+                    German & Chinese but continued learning & using French,
+                    Spanish & Polish.
                   </p>
                 </div>
               </div>
@@ -50,7 +61,10 @@ export const MainPage = () => {
                 <div className="card-body">
                   <h3>Work Experience</h3>
                   <p className="card-text">
-                    10+ years of own experience as an English speaker in business & IT, also working as a corporate soft skills trainer, gave me the perspective of how English is a tool, not a goal.
+                    10+ years of own experience as an English speaker in
+                    business & IT, also working as a corporate soft skills
+                    trainer, gave me the perspective of how English is a tool,
+                    not a goal.
                   </p>
                 </div>
               </div>
@@ -66,7 +80,13 @@ export const MainPage = () => {
                 <div className="card-body">
                   <h3>Why it's important to you</h3>
                   <p className="card-text">
-                    I encourage people to explore how English can be something they enjoy so much, they just want to use it all the time. Learning a new language is like meeting a new person, your new English-speaking identity. I will help you nurture that new confidence while keeping tabs on how to save your effort for this new marathon you'll be running for as long as English is part of your life.
+                    I encourage people to explore how English can be something
+                    they enjoy so much, they just want to use it all the time.
+                    Learning a new language is like meeting a new person, your
+                    new English-speaking identity. I will help you nurture that
+                    new confidence while keeping tabs on how to save your effort
+                    for this new marathon you'll be running for as long as
+                    English is part of your life.
                   </p>
                 </div>
               </div>
